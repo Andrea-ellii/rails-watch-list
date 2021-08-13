c12e80522e3f138250c645ca070829b38e81a617
Andrea Abbou <andrea.abbou@gmail.com>
2021-08-13 11:53:15 +0200

    config ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,19 @@
 class BookmarksController < ApplicationController
-  def index
-    @bookmarks = Bookmark.all
-  end
+  # def index
+  #   @bookmarks = Bookmark.all
+  # end
 
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
-    @movie = Movie.find(params[:bookmark][:movie_id])
+    # @movie = Movie.find(params[:bookmark][:movie_id])
     @bookmark.list = @list
-    @bookmark.movie = @movie
+    # @bookmark.movie = @movie
     if @bookmark.save
       redirect_to list_path(@list)
     else render :new
